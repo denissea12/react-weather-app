9ee43f0ba3964e2fc9a77877070cc21b0b258c77
denissea12 <denissea.12@gmail.com>
2025-04-08 07:25:17 -0500

    Added link to SheCodes profile and repository

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This Project was coded by{" "}
+        <a href="https://www.shecodes.io/graduates/131305-denisse-ramirez">
+          Denisse Ramirez
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/denissea12/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github.{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
