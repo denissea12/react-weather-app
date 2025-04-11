efa74c899648e80bfb830a7d53bd0c0a390e3fc2
denissea12 <denissea.12@gmail.com>
2025-04-10 21:27:33 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This Project was coded by{" "}
-        <a href="https://www.shecodes.io/graduates/131305-denisse-ramirez">
-          Denisse Ramirez
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/denissea12/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github.{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This Project was coded by{" "}
+          <a href="https://www.shecodes.io/graduates/131305-denisse-ramirez">
+            Denisse Ramirez
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/denissea12/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github.{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
